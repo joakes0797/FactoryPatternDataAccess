b88248f351c78d0afd95efa41f381a67fe52d846
Jill Oakes <jericho_rules@hotmail.com>
2022-04-05 15:28:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryPatternDataAccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
